3a32ece142a476d562b4ca97b590af7f19bd8c53
Ang Panha <angpanha2019@gmail.com>
2026-06-15 14:21:53 +0700

    feat: initial setup - bare

---
+++ src/main/java/kh/edu/itc/i4/devops_final/DevopsFinalApplication.java
@@ -0,0 +1,13 @@
+package kh.edu.itc.i4.devops_final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsFinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kh/edu/itc/i4/devops_final/DevopsFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package kh.edu.itc.i4.devops_final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
